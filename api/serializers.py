5d6618d8ed6bc8ae594f9f9a5e9bbc68283cbc41
knmarcin <Imspy4pcp@>
2021-10-28 12:47:11 +0200

    add date to closest car service based on last service date in car model

--- api/serializers.py
+++ api/serializers.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 from rest_framework import serializers
 from api.models import Employee, Car, History
 
@@ -8,6 +11,8 @@ class HistorySerializer(serializers.ModelSerializer):
     car_year = serializers.SerializerMethodField()
     worker = serializers.SerializerMethodField()
     car_registration = serializers.SerializerMethodField()
+    service_date = serializers.SerializerMethodField()
+    days_to_service = serializers.SerializerMethodField()
 
     def get_make_model(self, obj):
         return (f"{obj.car.make} {obj.car.model}")
@@ -21,6 +26,16 @@ class HistorySerializer(serializers.ModelSerializer):
     def get_car_registration(self, obj):
         return obj.car.registration
 
+    def get_service_date(self, obj):
+        return obj.car.service_date
+
+    def get_days_to_service(self, obj):
+        try:
+            new_date = datetime.date(obj.car.service_date.year + 1, obj.car.service_date.month, obj.car.service_date.day)
+            return (new_date - datetime.date.today()).days
+        except:
+            return 0
+
     class Meta:
         model = History
-        fields = ('id', 'make_model', 'car_year', 'car_registration', 'repair_log', 'worker' )
+        fields = ('id', 'make_model', 'car_year', 'car_registration', 'repair_log', 'worker', 'service_date', 'days_to_service' )
