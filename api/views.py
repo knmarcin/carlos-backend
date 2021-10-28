263e1e7a1fa4395fbc620725ebe3d08b0483f815
knmarcin <Imspy4pcp@>
2021-10-28 11:20:46 +0200

    add first view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from api.models import History
+from api.serializers import HistorySerializer
+from rest_framework import viewsets
 
-# Create your views here.
+class HistoryViewSet(viewsets.ModelViewSet):
+    queryset = History.objects.all()
+    serializer_class = HistorySerializer
