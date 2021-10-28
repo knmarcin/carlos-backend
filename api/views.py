04ccae8faf91bf48a6e82a1f27dade86f43cbef6
knmarcin <Imspy4pcp@>
2021-10-28 12:47:58 +0200

    add possibility of filtering

--- api/views.py
+++ api/views.py
@@ -1,7 +1,11 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from api.models import History
 from api.serializers import HistorySerializer
-from rest_framework import viewsets
+from rest_framework import generics, filters
 
-class HistoryViewSet(viewsets.ModelViewSet):
+class HistoryViewSet(generics.ListAPIView):
     queryset = History.objects.all()
     serializer_class = HistorySerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields =['repair_log', 'date_of_repair', 'employee__name']
+
