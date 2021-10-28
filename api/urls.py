567bb2144201c854c469d83ed7d4f2f1bbde252a
knmarcin <Imspy4pcp@>
2021-10-28 13:52:14 +0200

    add history/create/ endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
-from api.views import HistoryViewSet, HistoryDetailView
+from api.views import HistoryViewSet, HistoryDetailView, CreateHistoryView
 from django.urls import path
 
 
 urlpatterns = [
     path('history/', HistoryViewSet.as_view()),
+    path('history/create/', CreateHistoryView.as_view()),
     path('history/<int:pk>/', HistoryDetailView.as_view()),
 ]
