52e2c2b6425ad006e848df993c37e510654e912c
knmarcin <knapmarcin@icloud.com>
2021-11-16 12:49:03 +0100

    jwt change access token lifetime

--- carlos/settings.py
+++ carlos/settings.py
@@ -13,6 +13,7 @@ import dj_database_url
 import django_heroku
 import os
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -148,6 +149,10 @@ REST_FRAMEWORK = {
 
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=5)
 }
 
 django_heroku.settings(locals())
